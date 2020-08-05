d42e9eb13230f2341e14218af4b38242c466f74e
shirishapolati <63014785+shirishapolati@users.noreply.github.com>
2020-08-05 19:13:25 -0400

    Delete OutOfMemoryExample.java

--- src/main/java/OutOfMemoryExample.java
+++
@@ -1,8 +0,0 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-
-public class OutOfMemoryExample {
-    public static void main(String[] args) {
-        BufferedWriter bufferedWriter=new BufferedWriter(new FileWriter())
-    }
-}
